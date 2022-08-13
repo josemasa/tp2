f416493d4b6377b81a7b3cf51b5f763a91296a9d
jose marcelo salas <utnfrt.josemarcelosalas@gmail.com>
2022-08-12 21:28:52 -0300

    lo q hiciomos con los pibes

--- juegos/Scaloneta2/base-de-preguntas.js
+++ juegos/Scaloneta2/base-de-preguntas.js
@@ -1,57 +1,58 @@
 let baseDePreguntas = [
-    {
-        pregunta:"¿Cuántos goles hizo Messi en la selección?",
-        ayuda: "Es el mayor goleador de la albiceleste, seguido de Gabriel Omar Batistuta con 54 goles",
-        ayudaImg: "https://i.ibb.co/JzKVnwR/bati.webp",
-        imagen: "https://i.ibb.co/Rp2rSZX/messirve-1.webp",
-        respuesta: " - 86",
-        distractores: [" - 34", " - 102", " - 237"],
-
-    },
-    {
-        pregunta:"¿Cuál es el nombre de Scaloni?",
-        ayuda: "es parecido al nombre de Messi",
-        ayudaImg: "https://i.ibb.co/Y3K5jzQ/messi-scaloni-1304589.jpg",
-        imagen: "https://i.ibb.co/F7D2ZnV/scaloni-pagina-24-optjpeg.webp",
-        respuesta: " - Lionel",
-        distractores: [" - Leonel", "- Leonardo", "- Roberto"], 
-
-    },
-    {
-        pregunta:"¿Que copas del mundo ganó Argentina?",
-        ayuda: "La primera copa la ganó frente a Paises Bajos Holanda y la seguna fue frente a Alemania ",
-        ayudaImg: "https://i.ibb.co/SR7CtqW/maradona-1440x810-wmk.jpg",
-        imagen: "https://i.ibb.co/NZXb09T/escudo.jpg",
-        respuesta: "- Argentina 1978 y Mexico 1986",
-        distractores: ["- Uruguay 1930 y Mexico 1986", "- Italia 1990 y Brasil 2014", "- Francia 1998 y Alemania 2006 "],
-
-    },
-    {
-        pregunta:"¿Cual era uno de los apodos del Profe Alejandro Sabella?",
-        ayuda: "tenia el nombre del personaje de yogurisimo",
-        ayudaImg: "https://i.ibb.co/C5qK7Sy/pachorra.jpg",
-        imagen: "https://i.ibb.co/VtjXk8Z/alejandro-sabella.jpg",
-        respuesta: "- Pachorra",
-        distractores: ["- la Pulga", "- la Joya", "- General"],
-
-    },
-    {
-        pregunta:"¿Quién es el arquero Titular para el Mundial Qatar 2022?",
-        ayuda: "popularisó la frase mirá que te como, hermano!...",
-        ayudaImg: "https://i.ibb.co/T8g5zsV/dibu.jpg",
-        imagen: "https://i.ibb.co/qyG3ZJP/arqueros1200jpg.webp",
-        respuesta: "- Dibu Martinez",
-        distractores: ["- Franco Armani", "- Esteban Andrada", "- Juan Musso"],
-
-    },
-    {
-        pregunta:"¿Con qué equipo le toca a la selección el primer partido en el mundial de Qatar?",
-        ayuda: "Contra este país, la Selección jugó 11 veces, con 6 triunfos, 2 empates y 3 caídas.",
-        ayudaImg: "https://i.ibb.co/3cmyxjz/arg-arabia-png-242310155.webp",
-        imagen: "https://i.ibb.co/jVzGN1D/grupo-c.jpg",
-        respuesta: "- Arabia Saudita",
-        distractores: ["- Chile", "- Italia", "- Brasil"],
-
-    },
-
+  {
+    pregunta: "¿Cuántos goles hizo Messi en la selección?",
+    ayuda:
+      "Es el mayor goleador de la albiceleste, seguido de Gabriel Omar Batistuta con 54 goles",
+    ayudaImg: "https://i.ibb.co/JzKVnwR/bati.webp",
+    imagen: "https://i.ibb.co/Rp2rSZX/messirve-1.webp",
+    respuesta: "  86",
+    distractores: [" 34", " 102", "  237"],
+  },
+  {
+    pregunta: "¿Cuál es el nombre de Scaloni?",
+    ayuda: "es parecido al nombre de Messi",
+    ayudaImg: "https://i.ibb.co/Y3K5jzQ/messi-scaloni-1304589.jpg",
+    imagen: "https://i.ibb.co/F7D2ZnV/scaloni-pagina-24-optjpeg.webp",
+    respuesta: "  Lionel",
+    distractores: [" Leonel", " Leonardo", "Roberto"],
+  },
+  {
+    pregunta: "¿Que copas del mundo ganó Argentina?",
+    ayuda:
+      "La primera copa la ganó frente a Paises Bajos Holanda y la seguna fue frente a Alemania ",
+    ayudaImg: "https://i.ibb.co/SR7CtqW/maradona-1440x810-wmk.jpg",
+    imagen: "https://i.ibb.co/NZXb09T/escudo.jpg",
+    respuesta: " Argentina 1978 y Mexico 1986",
+    distractores: [
+      " Uruguay 1930 y Mexico 1986",
+      " Italia 1990 y Brasil 2014",
+      " Francia 1998 y Alemania 2006 ",
+    ],
+  },
+  {
+    pregunta: "¿Cual era uno de los apodos del Profe Alejandro Sabella?",
+    ayuda: "tenia el nombre del personaje de yogurisimo",
+    ayudaImg: "https://i.ibb.co/C5qK7Sy/pachorra.jpg",
+    imagen: "https://i.ibb.co/VtjXk8Z/alejandro-sabella.jpg",
+    respuesta: " Pachorra",
+    distractores: [" la Pulga", " la Joya", " General"],
+  },
+  {
+    pregunta: "¿Quién es el arquero Titular para el Mundial Qatar 2022?",
+    ayuda: "popularisó la frase mirá que te como, hermano!...",
+    ayudaImg: "https://i.ibb.co/T8g5zsV/dibu.jpg",
+    imagen: "https://i.ibb.co/qyG3ZJP/arqueros1200jpg.webp",
+    respuesta: " Dibu Martinez",
+    distractores: [" Franco Armani", " Esteban Andrada", " Juan Musso"],
+  },
+  {
+    pregunta:
+      "¿Con qué equipo le toca a la selección el primer partido en el mundial de Qatar?",
+    ayuda:
+      "Contra este país, la Selección jugó 11 veces, con 6 triunfos, 2 empates y 3 caídas.",
+    ayudaImg: "https://i.ibb.co/3cmyxjz/arg-arabia-png-242310155.webp",
+    imagen: "https://i.ibb.co/jVzGN1D/grupo-c.jpg",
+    respuesta: " Arabia Saudita",
+    distractores: [" Chile", " Italia", " Brasil"],
+  },
 ];
